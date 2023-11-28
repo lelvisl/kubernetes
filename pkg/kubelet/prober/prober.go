f3b119ea63d566bd5d262c5e09f2c35d493666d6
Казарян Никита Гарикович <nikita.kazaryan@mts.ru>
2023-11-28 20:40:58 +0300

    add grpc

--- pkg/kubelet/prober/prober.go
+++ pkg/kubelet/prober/prober.go
@@ -186,7 +186,7 @@ func (pb *prober) runProbe(ctx context.Context, probeType probeType, p *v1.Probe
 			service = *p.GRPC.Service
 		}
 		klog.V(4).InfoS("GRPC-Probe", "host", host, "service", service, "port", p.GRPC.Port, "timeout", timeout)
-		return pb.grpc.Probe(host, service, int(p.GRPC.Port), timeout)
+		return pb.grpc.Probe(host, service, int(p.GRPC.Port), nsPath, timeout)
 	}
 
 	klog.InfoS("Failed to find probe builder for container", "containerName", container.Name)
